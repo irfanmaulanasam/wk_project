20b0f5ad93b5929374efd4f653c920aacb616cf0
irfan maulanasam <alternatiflab@gmail.com>
2020-12-15 23:16:33 +0700

    add some content in job offer section

--- components/Job_Offer/index.vue
+++ components/Job_Offer/index.vue
@@ -4,18 +4,18 @@
       <div class="container" v-if="index % 2 == 0">
         <div class="row">
           <div class="column-66">
-            <h1 class="xlarge-font"><b>The App</b></h1>
+            <h1 class="xlarge-font"><b>{{data.name||'The App'}}</b></h1>
             <h1 class="large-font" style="color: MediumSeaGreen">
-              <b>Why buy it?</b>
+              <b>{{ data.reason||'Why buy it?'}}</b>
             </h1>
             <p>
-              <span style="font-size: 36px">Take photos like a pro.</span> You
-              should buy this app because lorem ipsum consectetur adipiscing
+              <span style="font-size: 36px">{{data.why ||'Take photos like a pro'}},</span>
+               {{ data.description ||`You should buy this app because lorem ipsum consectetur adipiscing
               elit, sed do eiusmod tempor incididunt ut labore et dolore magna
               aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-              laboris nisi ut aliquip ex ea commodo consequat.
+              laboris nisi ut aliquip ex ea commodo consequat.`}}
             </p>
-            <button class="button">Download Application</button>
+            <WhatsappButton />
           </div>
           <div class="column-33">
             <img
@@ -65,39 +65,48 @@ export default {
     return {
       images: [
         {
-          name: "",
+          name: "kanopy kain",
           img: "parasol",
           kind: "png",
+          reason:'kenapa kanopi kain',
+          why:'awning adalah komponen tambahan untuk mempercantik atau menambah estetika sebuah bangunan',
+          description:" entah itu rumah ataupun tempat bisnis. Namun fungsi utama dari Canopy Kain ini adalah memberikan perlindungan dari sinar matahari  dan air hujan untuk apapun yang berada dibawahnya. Di Indonesia, fungsi dari Canopy Kain ini sama seperti gongsol, namun yang membedakan tentunya dari bahan dan cara membuatnya. Canopy Kain juga bisa dipasang untuk garasi atau teras rumah dengan model dan desain yang lebih bebas dari pada Canopy Kain yang dipasang di jendela atau pintu bangunan. Selain itu pemakaian Canopy Kain atau awning kain ini juga sangat cocok dipasang di tempat-tempat bisnis, seperti distro, café, restoran, hotel, resort, butik, pertokoan dan lain-lain. Selain fungsi utamanya untuk menahan sinar matahari dan air hujan, Canopy Kain juga dapat menambah nilai estetika bangunan anda. Bentuk, model dan desain Canopy Kain bisa disesuaikan menurut selera anda supaya lebih serasi dengan bangunan, mulai dari minimalis, klasik hingga moderen. Warung Canopy menyediakan layanan jasa pembuatan dan pemasangan Canopy Kain untuk berbagai kebutuhan, baik itu pribadi ataupun bisnis, Braja Awning menggunakan bahan-bahan terbaik, teknisi berpengalaman dan tentunya dengan harga yang kompetitif. Berbagai pilihan jenis kualitas rangka dan bahan Canopy Kain bisa anda pilih sendiri sesuai selera. Cakupan layanan Braja Awning tidak hanya di wilayah Jakarta, Depok, Tanggerang, Bandung ataupun wilayah Jabodetabek melainkan seluruh Indonesia"
         },
         {
           name: "",
           img: "store_banne_lambrequin",
           kind: "png",
+          description:""
         },
         {
           name: "",
           img: "store_corbeille",
           kind: "png",
+          description:""
         },
         {
           name: "",
           img: "store_fixe",
           kind: "png",
+          description:""
         },
         {
           name: "",
           img: "store_projection",
           kind: "png",
+          description:""
         },
         {
           name: "",
           img: "store_veranda",
           kind: "png",
+          description:""
         },
         {
           name: "",
           img: "voile_d",
           kind: "png",
+          description:""
         },
       ],
     };
@@ -107,7 +116,7 @@ export default {
 
 <style scoped>
 #job-offer{
-  background-color: teal;
+  background-color: rgba(187, 248, 248, 0.726);
 }
 .container {
   padding: 4em;
